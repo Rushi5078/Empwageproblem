2d6106bf55af856f89b2790021b4cfdad58864aa
Rushi5078 <rushikeshb5078@gmail.com>
2021-07-28 19:18:07 +0530

    Added:EmpwageAttande feature

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -2,3 +2,19 @@
 
 
 echo "welcome to empwageproblem"
+
+
+#! /bin/bash -x
+#constant variable
+IS_PRESENT=1
+
+#variable
+randomCheck=$((RANDOM%2))
+
+#selection
+if [ $randomCheck -eq 1 ]
+then 
+      echo "Employe is present"
+else 
+       echo "Employe is absent"
+fi
